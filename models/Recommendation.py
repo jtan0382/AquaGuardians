fef6a60b44c4d921846425bdecfd56e8c8f239db
ZhouZhou0044 <zzho0044@student.monash.edu>
2024-10-05 21:14:00 +1000

    changes on controller

--- models/Recommendation.py
+++ models/Recommendation.py
@@ -1,31 +1,31 @@
-# from config import get_db_connection
+from config import get_db_connection
 import pandas as pd
 import pyodbc
 
-# Database configuration
-DATABASE_CONFIG = {
-    "DRIVER": "ODBC Driver 18 for SQL Server",
-    "SERVER": "tcp:tp04-gaurdians.database.windows.net,1433",
-    "DATABASE": "tp04-azure-db",
-    "UID": "tp04",
-    "PWD": "monash@5120",
-    "ENCRYPT": "yes",
-    "TRUST_SERVER_CERTIFICATE": "no",
-    "CONNECTION_TIMEOUT": 30,
-}
+# # Database configuration
+# DATABASE_CONFIG = {
+#     "DRIVER": "ODBC Driver 18 for SQL Server",
+#     "SERVER": "tcp:tp04-gaurdians.database.windows.net,1433",
+#     "DATABASE": "tp04-azure-db",
+#     "UID": "tp04",
+#     "PWD": "monash@5120",
+#     "ENCRYPT": "yes",
+#     "TRUST_SERVER_CERTIFICATE": "no",
+#     "CONNECTION_TIMEOUT": 30,
+# }
 
-def get_db_connection():
-    conn_str = (
-        f"Driver={DATABASE_CONFIG['DRIVER']};"
-        f"Server={DATABASE_CONFIG['SERVER']};"
-        f"Database={DATABASE_CONFIG['DATABASE']};"
-        f"Uid={DATABASE_CONFIG['UID']};"
-        f"Pwd={DATABASE_CONFIG['PWD']};"
-        f"Encrypt={DATABASE_CONFIG['ENCRYPT']};"
-        f"TrustServerCertificate={DATABASE_CONFIG['TRUST_SERVER_CERTIFICATE']};"
-        f"Connection Timeout={DATABASE_CONFIG['CONNECTION_TIMEOUT']};"
-    )
-    return pyodbc.connect(conn_str)
+# def get_db_connection():
+#     conn_str = (
+#         f"Driver={DATABASE_CONFIG['DRIVER']};"
+#         f"Server={DATABASE_CONFIG['SERVER']};"
+#         f"Database={DATABASE_CONFIG['DATABASE']};"
+#         f"Uid={DATABASE_CONFIG['UID']};"
+#         f"Pwd={DATABASE_CONFIG['PWD']};"
+#         f"Encrypt={DATABASE_CONFIG['ENCRYPT']};"
+#         f"TrustServerCertificate={DATABASE_CONFIG['TRUST_SERVER_CERTIFICATE']};"
+#         f"Connection Timeout={DATABASE_CONFIG['CONNECTION_TIMEOUT']};"
+#     )
+#     return pyodbc.connect(conn_str)
 
 
 
@@ -59,8 +59,8 @@ def fetch_merged_data():
     
     return table
 
-try:
-    table = fetch_merged_data()
-    print(table)
-except Exception as e:
-    print(f"An error occurred: {e}")
+# try:
+#     table = fetch_merged_data()
+#     print(table)
+# except Exception as e:
+#     print(f"An error occurred: {e}")
